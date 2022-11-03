0314ba3d730f8d0c2d7c392b4b91e4b2e15b56b5
theshieber <eli.github@shieber.com>
2022-11-02 21:23:12 -0400

    Added current progress. Now commiting what I have

---
+++ splinefilterPOC.py
@@ -0,0 +1,121 @@
+import numpy as np
+import wave
+import pyaudio
+from scipy.io import wavfile
+from scipy import interpolate
+import math
+import matplotlib.pyplot as plt
+
+MaxVal = 2147483647
+
+def clippingFunction(inSample):
+	threshold = MaxVal #maximum 24 bit output
+
+	outSample = threshold - threshold/(abs(inSample) + math.sqrt(threshold) + 1) ** 2
+
+	#return 1
+	return outSample * np.sign(inSample) #preserves sign
+
+
+def softClip(sampleArr):
+	numSamples = len(sampleArr)
+	sampleArrOut = [[0] * 2 for i in range(numSamples)]
+
+	for i in range(numSamples):
+		sampleArrOut[i][0] = clippingFunction(sampleArr[i][0])
+		sampleArrOut[i][1] = clippingFunction(sampleArr[i][1])
+
+	return sampleArrOut
+
+
+
+def main():
+	fileName = 'TestAudioIn/sinC2.wav'
+	sampleRate, sampleArr = wavfile.read(fileName)
+	sr, sFlat = wavfile.read('TestAudioIn/silence.wav')
+
+	#sampleArrClipped = softClip(sampleArr)
+
+	#wavfile.write("test.wav", sampleRate, np.array(sampleArrClipped)) #need to convert to a numpy array for this function
+	
+	graphSignal([sampleArr])
+	#graphSignal([sampleArr, sFlat])
+
+	#print("File Name:", fileName)
+	#print("Frame Rate:", sampleRate)
+	#print("Sample Array In:", sampleArr[0:100])
+	#print("Sample Array Out :", sampleArrClipped[0:100])
+
+def graphSignal(sampleArrs):
+
+	stepSize = 100
+
+	for sampleArr in sampleArrs:
+
+		extractedChannel0 = [item[0]/MaxVal for item in sampleArr] #scaling to (-1,1)
+		skipNValues = extractedChannel0[::stepSize]
+		linSpace = range(0, len(skipNValues))
+		interpolationSpace = np.arange(0, len(skipNValues)-1, .1)
+
+		print(list(interpolationSpace))
+		#print(len(linSpaceRep))
+		#print(len(extractedChannel0))
+
+		#print(list(linSpaceRep))
+
+		splineRep = interpolate.splrep(linSpace, skipNValues, s="0")
+		splineEval = interpolate.splev(interpolationSpace, splineRep, der=0)
+
+		#splineRep and splineEval arent matching up
+
+		plt.plot(skipNValues, marker = "o", linestyle = 'None')
+		plt.plot(splineEval, marker = "o")
+		plt.axis([0, 200, -1, 1])
+
+		#testing git
+	
+	plt.show()
+
+main()
+
+'''
+Cades Clipper
+	yOut = threshold - frac(threshold)(yIn +1)^power
+
+Sigmoid Clipper
+	yOut = (2*threshold)/1+e^(power*-yIn) - threshold
+
+Bounce Clipper:
+	Recursively mirrors yIn over threshold until yOut is inbetween the threshold values.
+'''
+
+
+
+
+'''
+The following is tests regarding using the wave library
+with wave.open('TestAudioIn/silence.wav', 'rb') as inFile:
+
+	print ( "Number of channels",inFile.getnchannels())
+	print ( "Sample width",inFile.getsampwidth())
+	print ( "Frame rate.",inFile.getframerate())
+	print ( "Number of frames",inFile.getnframes())
+	print ( "parameters:",inFile.getparams())
+
+	samplerate, data = wavfile.read('TestAudioIn/silence.wav')
+	
+	
+	frame = inFile.setpos(100)
+	f1 = inFile.readframes(1)
+	f1Int = int.from_bytes(f1, "big")
+
+	frame = inFile.setpos(50)
+	f2 = inFile.readframes(1)
+	f2Int = int.from_bytes(f2, "big")
+	
+
+	#print(frames)
+	#print( f1Int)
+	#print( f2Int)
+
+'''
