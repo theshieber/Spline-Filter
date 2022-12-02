1565db020c1118e0c6223f9e27d25db63b536bd0
theshieber <eli.github@shieber.com>
2022-12-02 17:32:11 -0500

    Actually got the proof of concept working

--- splinefilterPOC.py
+++ splinefilterPOC.py
@@ -7,7 +7,7 @@ import math
 import matplotlib.pyplot as plt
 
 #MaxVal = 2147483647
-MaxVal = 19
+MaxVal = 2147483647
 #found relavant blog post:
 #http://yehar.com/blog/?p=197
 
@@ -33,93 +33,53 @@ def softClip(sampleArr):
 
 
 def main():
-	fileName = 'TestAudioIn/sinC2.wav'
+	#testAudioIn = 'sinC2'
+	testAudioIn = 'flume test'
+	fileName = 'TestAudioIn/' + testAudioIn + '.wav'
 	sampleRate, sampleArr = wavfile.read(fileName)
-	sr, sFlat = wavfile.read('TestAudioIn/silence.wav')
+	
+	stepSize = 500
 
 	#sampleArrClipped = softClip(sampleArr)
 
 	#wavfile.write("test.wav", sampleRate, np.array(sampleArrClipped)) #need to convert to a numpy array for this function
 	
-	#graphSignal(sampleArr[:10])
-	#graphSignal([[i*2 + 1, i*2 + 1] for i in range(10)])
-	graphSignal([sampleArr, sFlat])
 
-	#print("File Name:", fileName)
-	#print("Frame Rate:", sampleRate)
+	for i in range(1, 500+1, 50):
+		(splineEval, skipNValues, linSpace) = applySpline(sampleArr, i)
+		wavfile.write("TestAudioOut/" "" + testAudioIn + "_" + str(int(i/50)) + ".mp3", sampleRate, np.array(splineEval)) #need to convert to a numpy array for this function
+
+	#graphSignal(sampleArr)
+	#graphSignal([[i*2 + 1, i*2 + 1] for i in range(10)])
+	#graphSignal([sampleArr, sFlat])
+
+	print("File Name:", fileName)
+	print("Frame Rate:", sampleRate)
 	#print("Sample Array In:", sampleArr[0:100])
 	#print("Sample Array Out :", sampleArrClipped[0:100])
 
-def graphSignal(sampleArrs):
-	print(sampleArrs)
+	graphSignal(sampleArr, stepSize)
 
-	stepSize = 2
+def applySpline(sampleArrs, stepSize):
 
-	#print(sampleArr)
-			
 	extractedChannel0 = list(map(lambda x: x[0]/MaxVal, sampleArrs))
-	print(extractedChannel0)
-	#extractedChannel0 = [item[0]/MaxVal for item in sampleArr] #scaling to (-1,1)
 	skipNValues = extractedChannel0[::stepSize]
-	print(skipNValues)
-	#linSpace = np.linspace(0, len(skipNValues), len(sampleArrs)/stepSize)
-	#interpolationSpace = np.linspace(0, len(skipNValues), len(sampleArrs))
-	#linSpace = np.linspace(0, len(skipNValues), 1)
-	#linSpace = range(0, len(extractedChannel0), stepSize)
+
 	linSpace = list(range(0, len(extractedChannel0), stepSize))
-	print(linSpace)
-	#interpolationSpace = np.linspace(0, len(extractedChannel0), len(extractedChannel0))
 	interpolationSpace = list(range(0, len(extractedChannel0)))
-
-	#print(list(interpolationSpace))
-	#print(len(linSpaceRep))
-	#print(len(extractedChannel0))
-
-	#print(list(linSpaceRep))
 
 	splineRep = interpolate.splrep(linSpace, skipNValues, s="0")
 	splineEval = interpolate.splev(interpolationSpace, splineRep)
-	print(splineEval)
 
-	#print(splineEval)
+	return (splineEval, skipNValues, linSpace)
 
-	#splineRep and splineEval arent matching up
+def graphSignal(sampleArrs, stepSize):
 
+	(splineEval, skipNValues, linSpace) = applySpline(sampleArrs, stepSize)
+
+	plt.plot(splineEval)
 	plt.plot(linSpace, skipNValues, marker = "x", linestyle = 'None')
-	plt.plot(splineEval, marker = "o")
-	plt.axis([0, 10, -1, 1])
-
-					#testing git
-					#testing git again
-
-
-	'''for i, sampleArr in enumerate(sampleArrs):
-					print(sampleArr)
-			
-					extractedChannel0 = list(map(lambda x: x/MaxVal, sampleArr))
-					#extractedChannel0 = [item[0]/MaxVal for item in sampleArr] #scaling to (-1,1)
-					skipNValues = extractedChannel0[::stepSize]
-					linSpace = range(0, len(skipNValues))
-					interpolationSpace = np.arange(0, len(skipNValues)-1, .1)
-			
-					#print(list(interpolationSpace))
-					#print(len(linSpaceRep))
-					#print(len(extractedChannel0))
-			
-					#print(list(linSpaceRep))
-			
-					splineRep = interpolate.splrep(linSpace, skipNValues, s="0")
-					splineEval = interpolate.splev(interpolationSpace, splineRep, der=0)
-			
-					#splineRep and splineEval arent matching up
-			
-					plt.plot(skipNValues, marker = "x", linestyle = 'None')
-					plt.plot(splineEval, marker = "o")
-					plt.axis([0, 200, -1, 1])
-			
-					#testing git
-					#testing git again
-			'''
+	plt.axis([0, 10000, -1, 1])
 	
 	plt.show()
 
